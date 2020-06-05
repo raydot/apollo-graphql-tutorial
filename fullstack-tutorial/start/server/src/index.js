29f18a29dcbe11e554a21e3acb047ffd455b8463
David Kanter <david.kanter@herodigital.com>
2020-06-05 10:33:03 -0700

    Finished with section 4.  Stopping for now.

--- fullstack-tutorial/start/server/src/index.js
+++ fullstack-tutorial/start/server/src/index.js
@@ -8,7 +8,27 @@ const UserAPI = require("./datasources/user");
 
 const store = createStore();
 
+const isEmail = require("isemail");
+
 const server = new ApolloServer({
+  // Function whose return value becomes the context argument passed to every resolver
+  /**
+   * Here's what the context function does:
+   *  1.  Obtain the value for Authorization header (if any) included in the incoming request
+   *  2.  Decode the value of the Authorization header
+   *  3.  If the decoded value resembles an email address, obtain user detauls for
+   *      that address and return an object that includes those details in the user field.
+   */
+  context: async ({ req }) => {
+    // Simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+    if (!isEmail.validate(email)) return { user: null };
+    // find user by email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
